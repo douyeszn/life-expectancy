02b4d6f1207dd472f6fc9c7b23a05c7dae841731
douyeszn <vom@andrew.cmu.edu>
2024-07-31 23:23:04 +0200

    feat: initiate Reg

--- src/Admin.java
+++ src/Admin.java
@@ -1,13 +1,18 @@
+import java.util.UUID;
+
 public class Admin extends User {
     public Admin(String email, String password, String role) {
         super(email, password, role);
+    }
+
+    public String initiateReg(String email){
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString());
     }
 
     public void login(String email, String password) {
 
     }
 
-    public void initiateReg(){
 
-    }
 }
