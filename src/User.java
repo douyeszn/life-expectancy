7ef485f132df97417a07f1ea0d9a76f368bea4a7
douyeszn <vom@andrew.cmu.edu>
2024-07-31 23:23:25 +0200

    feat: Role enum

--- src/User.java
+++ src/User.java
@@ -1,35 +1,11 @@
 public abstract class User {
     private String email;
     private String password;
-    private String role;
+    private Role role;
 
-    public User(String email, String password, String role){
+    public User(String email, String password, Role role){
         this.email = email;
         this.password = password;
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
         this.role = role;
     }
 
