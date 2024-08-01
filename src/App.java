a3a687e1717ff6e0fbc03855240f35787ca65238
attahiruj <attahiruj@gmail.com>
2024-08-01 14:29:20 +0100

    pages

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -8,5 +9,31 @@ public class App {
         String[] userItem = new String[]{"victor","miene","admin"};
 //        UserManager.addUser(userItem);
         UserManager.findUser("a");
+    }
+    
+        public static int homePage() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("********** HIV Life Expectancy **********");
+        System.out.println("1. Login");
+        System.out.println("2. New Registration");
+        System.out.println("3. Complete Registration");
+        System.out.println("*****************************************");
+        int input = scanner.nextInt();
+        scanner.close();
+        return input;
+    }
+
+    public static void loginPage() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("**************** Login ****************");
+        System.out.print("Enter email > ");
+        Credentials.email = scanner.nextLine();
+        System.out.println();
+        System.out.print("Enter password > ");
+        Credentials.password = scanner.nextLine();
+        System.out.println("***************************************");
+        scanner.close();
     }
 }
