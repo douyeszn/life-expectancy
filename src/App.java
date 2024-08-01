e14929543f80037dd36cb44806a5a0e1f43ea9c8
douyeszn <vom@andrew.cmu.edu>
2024-08-01 15:01:33 +0200

    feat: patient can complete registration

--- src/App.java
+++ src/App.java
@@ -5,8 +5,20 @@ public class App {
         System.out.println("Enter username and password to login:");
 
         //test
-        String[] userItem = new String[]{"victor","miene","admin"};
-//        UserManager.addUser(userItem);
-        UserManager.findUser("a");
+          String[] userItem = new String[]{"victor","miene","admin"};
+          UserManager.completeRegistration(
+                  "4174000",
+                  "John",
+                  "Doe",
+                  "1990-01-01",
+                  true,
+                  false,
+                  "US",
+                  "2024-01-01",
+                  "securePassword123"
+          );
+
+
+//        UserManager.findUser("a");
     }
 }
--- src/UserManager.java
+++ src/UserManager.java
@@ -3,20 +3,53 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-
+import java.text.SimpleDateFormat;
 
 public class UserManager {
+    public static void completeRegistration(
+            String uuid,
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            boolean isHIVPositive,
+            boolean onARTMedication,
+            String countryISO,
+            String startARTDate,
+            String password) throws IOException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy\"");
+
+        String[] userParams = new String[]{
+                uuid,
+                firstName,
+                lastName,
+                Role.PATIENT.toString(),
+              dateOfBirth,
+                Boolean.toString(isHIVPositive),
+                Boolean.toString(onARTMedication),
+                countryISO,
+                startARTDate,
+                password
+        };
+
+        UserManager.addUser(userParams);
+    }
+
+
     public static String findUser(String email) throws IOException, InterruptedException {
 
         String[] cmd = new String[]{"resources/findUser.sh","victor"};
-
         ProcessBuilder pb = new ProcessBuilder(cmd);
         Process process = pb.start();
-        System.out.println("Got here");
+
+        process.waitFor();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String user = reader.readLine();
-        System.out.println("user: " + user);
+        String user;
+        while((user=reader.readLine())!= null) {
+            System.out.println(user);
+        }
         return user;
     }
 
@@ -31,7 +64,7 @@ public class UserManager {
         try{
             Process process = pb.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader( process.getInputStream()));
-            String s = null;
+            String s;
             while((s=reader.readLine())!= null) {
                 System.out.println(s);
             }
