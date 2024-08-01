e3021830401ed67f16a66597e8feecb8e6d25340
douyeszn <vom@andrew.cmu.edu>
2024-08-01 12:09:31 +0200

    feat: find and add user

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Enter username and password to login:");
 
         //test
-//        String[] userItem = new String[]{"victor","miene","admin"};
-//        FileManager.appendToFile(userItem);
+        String[] userItem = new String[]{"victor","miene","admin"};
+//        UserManager.addUser(userItem);
+        UserManager.findUser("a");
     }
 }
--- src/UserManager.java
+++ src/UserManager.java
@@ -5,22 +5,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UserManager {
-//    public User findUser(String email){
-//        grep Kigali data-no-header.csv | cut -d "," -f3
-//    }
 
-    public static void addUser(String[] userString){
-        String[] cmd = new String[]{"resources/test.sh"};
+public class UserManager {
+    public static String findUser(String email) throws IOException, InterruptedException {
+
+        String[] cmd = new String[]{"resources/findUser.sh","victor"};
+
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        Process process = pb.start();
+        System.out.println("Got here");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String user = reader.readLine();
+        System.out.println("user: " + user);
+        return user;
+    }
+
+    public static void addUser(String[] userString) throws IOException {
+      String[] cmd = new String[]{"resources/addUser.sh"};
         List<String> listOfArgs = new ArrayList<>();
-        listOfArgs.add("resources/test.sh");
+        listOfArgs.add("resources/addUser.sh");
 
         listOfArgs.addAll(Arrays.asList(userString));
         String[] args = listOfArgs.toArray(new String[0]);
         ProcessBuilder pb = new ProcessBuilder(args);
         try{
-            Process p = pb.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader( p.getInputStream()));
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader( process.getInputStream()));
             String s = null;
             while((s=reader.readLine())!= null) {
                 System.out.println(s);
