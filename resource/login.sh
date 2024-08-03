848753178d2895594f60bd2832382c1955981f10
douyeszn <vom@andrew.cmu.edu>
2024-08-03 15:20:14 +0200

    feat: hash password

--- resource/login.sh
+++ resource/login.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-email=$1
+email="$1"
 pwd=$2
 role=$3
 
-userRow=$(grep "^.*,$email,.*$" ./user-store.txt)
+userRow=$(awk -F',' '$4 == "'"$email"'" {print $0}' ./user-store.txt)
 
 if [ -z "$userRow" ]; then
-  echo "Login failed: Email not found"
+  echo "Login failed: UUID not found"
   exit 1
 fi
 
-uuid=$(echo "$userRow" | cut -d ',' -f1)
 storedPwd=$(echo "$userRow" | cut -d ',' -f5)
+
+salt="gishwati"
+hashPwd=$(openssl passwd -6 -salt "$salt" "$pwd")
 
 storedRole=$(echo "$userRow" | cut -d ',' -f6)
 
 if [ "$role" != "$storedRole" ]; then
-  echo "User does not exist"
+  echo "User does not exist."
   exit 0
 fi
 
-if [ "$pwd" == "$storedPwd" ]; then
-  echo "Login successful for UUID: $uuid"
-#  echo "$uuid $role"
+echo  "hash $hashPwd $storedPwd"
+if [ "$hashPwd" == "$storedPwd" ]; then
+  echo "Login successful for UUID: $1"
   exit 0
 else
   echo "Incorrect password"
