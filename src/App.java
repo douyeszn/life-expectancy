791db0fb0a600779b9780b7d56a92e6d1ffc6e21
hiro <attahiruj@gmail.com>
2024-08-08 11:25:07 +0200

    removed hello

--- src/App.java
+++ src/App.java
@@ -31,7 +31,7 @@ public class App {
                             User.getDataField(userData, DataStructure.email.getValue()),
                             User.getDataField(userData, DataStructure.password.getValue())
                             );
-                        admin.addDataField("hello", 2);
+                        // admin.addDataField("hello", 2);
                         Menus.adminPage(scanner, admin);
                     }
                     break;
